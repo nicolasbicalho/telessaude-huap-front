d967f681cf3eb795ac1c18e8ceb5813931f891c4
Nicolas Bicalho <88749176+nicolasbicalhocosta@users.noreply.github.com>
2023-05-04 21:04:54 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 })
+</script>
