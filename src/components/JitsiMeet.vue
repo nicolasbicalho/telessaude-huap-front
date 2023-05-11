3b94858801c921dc3a3a74b9b952f309c58295c1
Nicolas Bicalho <88749176+nicolasbicalhocosta@users.noreply.github.com>
2023-05-10 23:38:09 -0300

    feat: new jitsi component ONGOING

---
+++ src/components/JitsiMeet.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="jitsi-meet">
+    <JitsiMeet
+        ref="jitsiRef"
+        domain="meet.jit.si"
+        :options="jitsiOptions"
+    />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+import { JitsiMeet } from '@mycure/vue-jitsi-meet';
+
+const roonName = ref('');
+
+const jitsiOptions = computed(() => {
+    return {
+    roomName: 'some-room-name',
+    noSSL: false,
+    userInfo: {
+        email: 'user@email.com',
+        displayName: '',
+    },
+    configOverwrite: {
+        enableNoisyMicDetection: false
+    },
+    interfaceConfigOverwrite: {
+        SHOW_JITSI_WATERMARK: false,
+        SHOW_WATERMARK_FOR_GUESTS: false,
+        SHOW_CHROME_EXTENSION_BANNER: false
+    },
+    onload: onIFrameLoad
+    };
+});
+
+function onIFrameLoad() {
+
+}
+
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
