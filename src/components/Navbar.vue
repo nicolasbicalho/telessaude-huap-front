31e5d7d11f7028a2e07997fe0974644025a1c6bb
Nicolas Bicalho <88749176+nicolasbicalhocosta@users.noreply.github.com>
2023-05-21 19:50:20 -0300

    feat: new navbar page ONGOING

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, defineProps } from 'vue';
+import moment from 'moment';
+
+const props = defineProps(['email'])
+
+</script>
+
+<template>
+  <nav class="navbar">
+    <div>
+        {{ props.email }}
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+  .navbar {
+    padding: 0.5rem 1rem;
+    background-color: rgb(50, 50, 50);
+    color: white;
+    display: flex;
+  }
+</style>
